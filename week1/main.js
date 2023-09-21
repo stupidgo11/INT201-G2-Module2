fdc7d8c4ef539e768ede91720830cce1cefe954c
Natthaphol Nirattisaikul <zerozkiller@gmail.com>
2023-09-21 10:10:44 +0700

    third

--- week1/main.js
+++
@@ -1,6 +0,0 @@
-//create object with object literals
-const st1={studentId:65130500105, firstname:'Natthaphol',lastname:'Nirattisaikul'}
-console.log(typeof st1)
-console.log(st1)
-const st2 = {} //empty object
-console.log(st2)
