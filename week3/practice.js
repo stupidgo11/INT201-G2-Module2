edf024ca23e0cd7f8011a6351f73176882c558ec
Natthaphol <zerozkiller@gmail.com>
2023-10-05 09:23:20 +0700

    1st

---
+++ week3/practice.js
@@ -0,0 +1,15 @@
+function getPassingNames(students) {
+  return students
+    .filter((students) => students.score >= 70)
+    .map((filterStudents) => filterStudents.name.toUpperCase())
+}
+
+const studentScores = [
+  { name: 'Alice', score: 85 },
+  { name: 'Bob', score: 92 },
+  { name: 'Charlie', score: 68 },
+  { name: 'David', score: 55 },
+  { name: 'Eve', score: 78 }
+]
+const passingNames = getPassingNames(studentScores)
+console.log(passingNames)
