9c92698dd28aecb7769cbde6712a7b427c94d1eb
Natthaphol Nirattisaikul <zerozkiller@gmail.com>
2023-11-22 12:25:46 +0700

    w14

--- week14/main.js
+++ week14/main.js
@@ -1,15 +1,36 @@
 const submitButton = document.getElementById('submit-btn-01')
 submitButton.addEventListener('click', (e) => {
   e.preventDefault()
-  const allInputEles = document.querySelectorAll('input')
-  const validInput = Array.from(allInputEles).every((e) => e.value.length !== 0)
-  const pEle = document.querySelector('p')
-  if (!validInput) {
-    pEle.textContent = 'missing username or password'
-    pEle.style = 'color:red'
-  } else {
-    pEle.textContent = 'success'
-    pEle.style = 'color:green'
-  }
   console.log('submit clicked')
+  const inputElements = document.querySelectorAll('input')
+  //   console.log(inputElements[0].value)
+  //   console.log(inputElements[1].value)
+  const pElement = document.querySelector('p')
+  if (
+    inputElements[0].value.length === 0 ||
+    inputElements[1].value.length === 0
+  ) {
+    pElement.textContent = 'some values are missing, please check'
+  } else {
+    pElement.textContent = 'Your input are complete'
+  }
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('DOM is loaded')
+})
+window.addEventListener('load', () => {
+  console.log('Load')
+})
+window.addEventListener('beforeunload', () => {
+  console.log('before unload')
+  localStorage.setItem('myCat', 'Tom')
+})
+
+const inputElements = document.querySelectorAll('input')
+inputElements[0].addEventListener('focus', () => {
+  console.log('input focused')
+})
+inputElements[0].addEventListener('blur', () => {
+  console.log('input blured')
 })
